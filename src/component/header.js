b651f8514d62bc64b7af43535a200f39a2ecf1d9
olumayokunolayinka <olumayokunolayinka@gmail.com>
2020-09-06 14:15:59 +0100

    update

--- src/component/header.js
+++ src/component/header.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 const Header = () => {
     return(
         <div>
-            Header
+            <nav className="navbar navbar-light navbar-expand-md">
+                <div className="container"><Link className="navbar-brand" to={'/'}>eMarhz</Link>
+                    <button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span
+                        className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"/>
+                    </button>
+                    <div className="collapse navbar-collapse"
+                         id="navcol-1">
+                        <ul className="nav navbar-nav">
+                            <li className="nav-item" role="presentation"><Link className="nav-link" to={'/'}>Home</Link></li>
+                            <li className="nav-item" role="presentation"><Link className="nav-link" to={'/'}>Contact</Link>
+                            </li>
+                        </ul>
+                        <ul className="nav navbar-nav ml-auto">
+                            <li className="nav-item" role="presentation"><a className="nav-link" href="#"><i
+                                className="material-icons">person</i></a></li>
+                            <li className="nav-item shopping-cart" role="presentation"><a className="nav-link" href="#"><i
+                                className="material-icons">shopping_cart</i><span>2</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 };
