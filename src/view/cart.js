288c17d85fbc342fd90b14a65d4f57eac3281b0e
olumayokun olayinka <41644496+kodedict@users.noreply.github.com>
2020-09-07 18:02:37 +0100

    checkout / Payment
    Cart page updated
    Paystack installed

--- src/view/cart.js
+++ src/view/cart.js
@@ -7,7 +7,47 @@ const Cart = () => {
     return (
         <div>
             <HeaderSection />
-            <p>Cart</p>
+            <div className="mt-50">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-8">
+                    <div className="table-responsive">
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Image</th>
+                                    <th>Product Name</th>
+                                    <th>Quantity</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><img className="img-fluid" alt={'/'} /></td>
+                                    <td>Cell 2</td>
+                                    <td>Cell 3</td>
+                                    <td>
+                                        <ul className="list-inline">
+                                            <li className="list-inline-item"><button class="btn btn-primary" type="button" style={{backgroundcolor: "rgb(176,9,79)",border: "none",borderRadius: "0"}}><i className="material-icons">add</i></button></li>
+                                            <li className="list-inline-item"><button class="btn btn-primary" type="button" style={{backgroundColor: "rgb(1,13,25)",border: "none",borderRadius: "0"}}><i className="material-icons">remove</i></button></li>
+                                        </ul>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="col-md-4"><span>Total item(s)</span>
+                    <p>1</p><span>Amount</span>
+                    <p>N 2000</p>
+                    <form>
+                        <div className="form-group"><input className="form-control" type="email" placeholder="Email" style={{height: "50px",borderRadius: "0",border: "1px solid rgb(0,6,11)"}}/></div>
+                        <div><button className="btn btn-primary" type="button" style={{backgroundColor: "rgb(0,6,11)",height: "50px",width: "100%",border: "none",borderRadius: "0"}}>Pay with paystack</button></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
         </div>
     )
  }
